4610ebae7d4d52b6f8a9eea789677edf161c7e15
kentaczi <kentaczifrajedcziken@gmail.com>
2023-04-11 23:54:19 +0200

    pokoj zajecia

--- src/main/java/Pokoj.java
+++ src/main/java/Pokoj.java
@@ -6,6 +6,22 @@ public class Pokoj {
         this.pietro = pietro;
     }
 
+    public boolean removeZajecia() {
+        if (zajecia != null) {
+            zajecia.clearPokoj();
+            clearZajecia();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean clearZajecia() {
+        if (zajecia == null)
+            return false;
+        this.zajecia = null;
+        return true;
+    }
+
     public boolean setZajecia(Zajecia zajecia) {
         if (this.zajecia == null || !this.zajecia.equals(zajecia)) {
             this.zajecia = zajecia;
--- src/main/java/Zajecia.java
+++ src/main/java/Zajecia.java
@@ -21,8 +21,34 @@ public class Zajecia {
         return false;
     }
 
-    public void addStudent(Student student) {
+    public boolean clearPokoj() {
+        if (this.pokoj == null)
+            return false;
+        this.pokoj = null;
+        return true;
+    }
+
+    public boolean removePokoj() {
+        if (this.pokoj != null) {
+            pokoj.clearZajecia();
+            clearPokoj();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addStudent(Student student) {
+        if (this.listaStudentow.contains(student))
+            return false;
         this.listaStudentow.add(student);
+        return true;
+    }
+
+    public boolean removeStudent(Student student) {
+        if (!this.listaStudentow.contains(student))
+            return false;
+        this.listaStudentow.remove(student);
+        return true;
     }
 
 }
