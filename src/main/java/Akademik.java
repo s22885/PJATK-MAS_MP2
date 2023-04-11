c5a75ac33b07519305e4613b9bf3056a1c88a486
kentaczi <kentaczifrajedcziken@gmail.com>
2023-04-11 23:38:16 +0200

    akademik

--- src/main/java/Akademik.java
+++ src/main/java/Akademik.java
@@ -7,9 +7,25 @@ public class Akademik {
 
     public Akademik(String nazwaAkademika) {
         this.nazwaAkademika = nazwaAkademika;
-        this.pokoje = new HashMap<Integer, Pokoj>();
+        this.pokoje = new HashMap<>();
     }
-    public void addPokoj(int id, Pokoj pokoj){
-        this.pokoje.put(id,pokoj);
+
+    public boolean addPokoj(int id, Pokoj pokoj) {
+        if (this.pokoje.containsValue(pokoj)) return false;
+
+        this.pokoje.put(id, pokoj);
+        return true;
+    }
+
+    public boolean removePokoj(int id) {
+        if (!this.pokoje.containsKey(id)) return false;
+        this.pokoje.remove(id);
+        return true;
+    }
+
+    public boolean removePokoj(Pokoj pokoj) {
+        if (!this.pokoje.containsValue(pokoj)) return false;
+        this.pokoje.values().remove(pokoj);
+        return true;
     }
 }
